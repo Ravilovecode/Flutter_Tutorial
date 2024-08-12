d9800361fbc6b2be7fbcea4ef3416dbd5c5b11fc
Ravi Kumar Ray <121496417+Ravilovecode@users.noreply.github.com>
2024-08-12 17:54:45 +0530

    "new changed code for hot reload support"

--- main.dart
+++ main.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter/material.dart';
-
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'hello flutter app',
-          style: TextStyle(color: Colors.white), 
-        ),
-        backgroundColor: Color.fromARGB(255, 243, 224, 56),
-      ),
-      body: Center(
-        child: Text('hello flutter'),
-      ),
-    ),
-  ));
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello Flutter App"),
+          backgroundColor: Color.fromARGB(255, 63, 194, 183),
+        ),
+        body: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.yellow, // Set the background color here
+              borderRadius: BorderRadius.circular(8.0), // Set the border radius here
+            ), // Optional: Add padding for better appearance
+            child: Text(
+              "Welcome to my Hello Flutter App!",
+              style: TextStyle(fontSize: 24), // Optional: Adjust text style
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
